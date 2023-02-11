6654740534586adbbd45606bbfd679d557037805
wukongyong <1334306978@qq.com>
2023-02-11 16:12:14 +0800

    learn and record

--- src/main/java/wkyyzl/cn/MainApplication.java
+++ src/main/java/wkyyzl/cn/MainApplication.java
@@ -7,21 +7,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class MainApplication {
 
-    static ConfigurableApplicationContext context;
-
     public static void main(String[] args) {
         //1.返回ioc容器
-        context = SpringApplication.run(MainApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
 
         //2.查看容器里面的组件
-        String[] names = context.getBeanDefinitionNames();
+        String[] names = run.getBeanDefinitionNames();
         for(String s : names){
             System.out.println(s);
         }
-    }
-
-    public ConfigurableApplicationContext test(){
-        return context;
     }
 
 }
---
+++ src/test/java/wkyyzl/cn/RedisTests.java
@@ -0,0 +1,46 @@
+package wkyyzl.cn;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import wkyyzl.cn.bean.Person;
+import wkyyzl.cn.util.RedisUtils;
+
+@SpringBootTest
+public class RedisTests {
+
+    @Autowired
+    Person person;
+
+    @Resource
+    RedisTemplate redisTemplate;
+
+    @Autowired
+    RedisUtils redisUtils;
+
+    @Test
+    void redisTest(){
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+        operations.set("hello", "world");
+        String hello = operations.get("hello");
+        System.out.println(hello);
+    }
+
+    @Test
+    void redisTest2(){
+        redisUtils.set("hello", "world");
+        String hello = (String) redisUtils.get("hello");
+        System.out.println(hello);
+    }
+
+    @Test
+    void contextLoads() {
+        redisUtils.set(person.getUserName(), person);
+        Person getPerson = (Person) redisUtils.get(person.getUserName());
+        System.out.println(getPerson);
+    }
+
+}
