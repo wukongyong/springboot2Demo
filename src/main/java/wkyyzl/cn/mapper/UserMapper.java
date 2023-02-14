47605a2f014bab1446fc2b237f1c875f5664f168
wukongyong <1334306978@qq.com>
2023-02-14 17:31:58 +0800

    learn and record

---
+++ src/main/java/wkyyzl/cn/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package wkyyzl.cn.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import wkyyzl.cn.bean.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ src/test/java/wkyyzl/cn/MyBatisPlusTests.java
@@ -0,0 +1,61 @@
+package wkyyzl.cn;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import wkyyzl.cn.bean.User;
+import wkyyzl.cn.mapper.UserMapper;
+
+import java.util.List;
+
+@SpringBootTest
+public class MyBatisPlusTests {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Test
+    void getAllUsers(){
+        List<User> users = userMapper.selectList(null);
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    void insertUser(){
+        User user = new User();
+        user.setUserName("wky");
+        user.setAddress("address");
+        user.setAge(24);
+        user.setName("一二");
+        user.setPassword("fdd");
+        user.setMail("fjiwo&qq.com");
+
+        userMapper.insert(user);
+    }
+
+    // 测试分页查询
+    @Test
+    public void testSelectPage(){
+        Page<User> page = new Page<>(1,2); //查询第一页，查询1条数据
+
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        //设置查询条件
+        //wrapper.like("email", "qq.com");
+
+        IPage<User> iPage = this.userMapper.selectPage(page, null);
+        System.out.println("数据总条数： " + iPage.getTotal());
+        System.out.println("数据总页数： " + iPage.getPages());
+        System.out.println("当前页数： " + iPage.getCurrent());
+
+        List<User> records = iPage.getRecords();
+        for (User record : records) {
+            System.out.println(record);
+        }
+    }
+
+}
